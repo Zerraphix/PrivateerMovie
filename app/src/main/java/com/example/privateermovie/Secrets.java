88015034dcbaaed0461d64b268ef61cdb462d9db
Joey Knutsson <joey.knutsson@rocketmail.com>
2024-04-24 13:21:30 +0200

    Secret

---
+++ app/src/main/java/com/example/privateermovie/Secrets.java
@@ -0,0 +1,7 @@
+package com.example.privateermovie;
+
+public class Secrets {
+
+    public static final String Token = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4Zjg3ZTVjYjhmNDY1ZTk0MjViMDc5ZWJmOWViZGVlOCIsInN1YiI6IjY2MjYwNGUyMjU4ODIzMDE3ZDkyOGUyMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.NPs6rE6gkzc4JubfcCr6ssuvfChImEWJzqVuhA3gc1A";
+    public static final String API_KEY = "8f87e5cb8f465e9425b079ebf9ebdee8";
+}
